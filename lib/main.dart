6ec6731c74a6e72c4a16d1d21f5a43db2035d316
Izan Bueno <75090205+Izzaaann@users.noreply.github.com>
2025-02-26 20:23:59 +0100

    refactor main app structure and update title for T-shirt calculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'main_2.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Calculadora de Samarretes',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-    );
-  }
+      home: const TShirtCalculatorScreen(),
+    );
+  }
 }
